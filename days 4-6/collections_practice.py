cec49046fe8869c53a42bd912ae8e7c00abfa4a3
max <max@treesfromspace.com>
2020-01-13 21:09:35 -0500

    deque example from python cookbook, keeps last few items of an interation

--- days 4-6/collections_practice.py
+++ days 4-6/collections_practice.py
@@ -8,3 +8,19 @@ CarModel = namedtuple('Owners', 'ID, FirstName, LastName, Make, Model')
 
 for owner in map(CarModel._make, csv.reader(open('/Users/max/PycharmProjects/100-days-of-code/days 4-6/mock_data.csv', 'rt'))):
     print(owner.FirstName, owner.Make, owner.Model)
+
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield lines, previous_lines
+        previous_lines.append(line)
+
+if __name__=='__main__':
+    with open('somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+                print(pline, end='')
+                print('-'*20)
