91da15338ea93b6a72251f555ca94c7a68577e0b
maxmarzolf <max@treesfromspace.com>
2020-09-29 14:16:56 -0400

    determined winner TODO: add error handling for incorrect user inputs loop game and set loop game limits (best 3 out of 5 etc) make output more descriptive and...THEATRICAL!

--- days 13-15/day14.py
+++ days 13-15/day14.py
@@ -18,7 +18,36 @@ def main():
 def start_game():
     opponent = Computer('')
     opponents_choice = opponent.choose_state()
-    print(opponents_choice)
+    player = players_choice()
+    determine_winner(opponents_choice, player)
+
+
+def players_choice():
+    choice = input()
+    return choice
+
+
+def determine_winner(opponent, player):
+    if opponent == 'rock' and player == 'scissors':
+        print('you loose - rock smashes scissors')
+    if opponent == 'paper' and player == 'rock':
+        print('you loose - paper covers rock')
+    if opponent == 'scissors' and player == 'paper':
+        print('you loose - scissors cuts paper')
+
+    if opponent == 'rock' and player == 'paper':
+        print('you win - paper covers rock')
+    if opponent == 'paper' and player == 'scissors':
+        print('you win - scissors cuts paper')
+    if opponent == 'scissors' and player == 'rock':
+        print('you win - rock smashes scissors')
+
+    if opponent == 'rock' and player == 'rock':
+        print('tie')
+    if opponent == 'paper' and player == 'paper':
+        print('tie')
+    if opponent == 'scissors' and player == '=scissors':
+        print('tie')
 
 
 if __name__ == '__main__':
