ec03e47fc14f10b3e1491129d039399116a93588
maxmarzolf <max@treesfromspace.com>
2020-10-02 11:29:04 -0400

    simple list comprehension

---
+++ days 16-18/day17.py
@@ -0,0 +1,13 @@
+names = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+capitalized_names = [name.title() for name in names]
+print(capitalized_names)
+
+
+# create generator
+# pairs = gen_pairs()
+# for _ in range(10):
+#     next(pairs)
