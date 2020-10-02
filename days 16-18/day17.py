9e590f6957b3b682003fcc41ebcf222012a75510
maxmarzolf <max@treesfromspace.com>
2020-10-02 17:33:56 -0400

    TODO: properly loop generator

--- days 16-18/day17.py
+++ days 16-18/day17.py
@@ -1,3 +1,5 @@
+import random
+
 names = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
@@ -7,7 +9,14 @@ capitalized_names = [name.title() for name in names]
 print(capitalized_names)
 
 
-# create generator
-# pairs = gen_pairs()
-# for _ in range(10):
-#     next(pairs)
+def gen_pairs():
+    first_choice = random.choice(names)
+    second_choice = random.choice(names)
+    pair = first_choice + ' is working with ' + second_choice
+    print(pair)
+    yield pair
+
+
+my_first_generator = gen_pairs()
+for _ in range(10):
+    next(my_first_generator)
