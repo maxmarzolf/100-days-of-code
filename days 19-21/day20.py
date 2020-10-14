fbac934734837d4b9b24c5f4f094ae6336dc84d5
maxmarzolf <max@treesfromspace.com>
2020-10-14 11:38:58 -0400

    use of cycle

--- days 19-21/day20.py
+++ days 19-21/day20.py
@@ -1,4 +1,9 @@
-import time
+from time import sleep
+import itertools
+
+
+colors = 'Red Green Yellow'.split()
+rotation = itertools.cycle(colors)
 
 
 def traffic_light(traffic_lights):
@@ -7,8 +12,22 @@ def traffic_light(traffic_lights):
         current_state.append(light)
     while current_state:
         for light in current_state:
-            time.sleep(4)
+            sleep(4)
             print(light)
 
 
-traffic_light(['green', 'yellow', 'red'])
+def light_sequence(rotation):
+    for color in rotation:
+        if color == 'Yellow':
+            print('Caution')
+            sleep(3)
+        elif color == 'Red':
+            print('Stop')
+            sleep(3)
+        elif color == 'Green':
+            print('Go')
+            sleep(3)
+
+
+# traffic_light(['green', 'yellow', 'red'])
+light_sequence(rotation)
